15d60f2d319da86e1cb79b1a2ff3977475d218f9
Julius Philip Bartolome <juliusbartolome@hotmail.ph>
2018-09-03 02:28:20 +0800

    added working sample of angular js

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+import { filter } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent implements OnInit {
+
+  currentUrl: string;
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((_: NavigationEnd) => this.currentUrl = _.url);
+  }
+
+}
